26db7bae706acc628ff185abacde97fc7356bd03
Karl Kallavus <karl.kallavus@gmail.com>
2016-10-12 19:57:57 +0300

    minor pep8 fix

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 
 
@@ -6,19 +8,18 @@ app = Flask(__name__)
 
 
 @app.route("/cars", methods=["GET"])
-def list_cars():	
-	return "Cars list will appear here"
+def list_cars():
+    return "Cars list will appear here"
 
 
 @app.route("/car/<string:id>", methods=["GET"])
 def car_by_id(id):
-	abort(404)
+    abort(404)
 
 
 @app.route("/car/<string:id>/price/<string:new_price>", methods=["PUT"])
-def update_car_price(id, new_price):	
-	return "New price is" + new_price
+def update_car_price(id, new_price):
+    return "New price is" + new_price
 
-	
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)

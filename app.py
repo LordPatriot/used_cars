96890aa23f97a21554589e805f443e732df41863
Vladimir Orekhov <vladimir.orekhov@booking.com>
2016-10-12 22:41:40 +0200

    Serialize/desiralize dummy car

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask import request
+from flask import jsonify
+import Car_pb2
+import uuid
 
 
 # create the Flask application
@@ -12,14 +16,27 @@ def list_cars():
     return "Cars list will appear here"
 
 
-@app.route("/car/<string:id>", methods=["GET"])
-def car_by_id(id):
-    abort(404)
+@app.route("/car/new", methods=["POST"])
+def create_car():
+    mark = request.args.get('mark')
+    model = request.args.get('model')
+    year = int(request.args.get('year'))
+
+    car = Car_pb2.Car()
+    car.mark = mark
+    car.model = model
+    car.year = year
+
+    # for now let's throw bytes
+    buf = car.SerializeToString()
+
+    return buf
 
 
 @app.route("/car/<string:id>/price/<string:new_price>", methods=["PUT"])
 def update_car_price(id, new_price):
     return "New price is" + new_price
+
 
 if __name__ == "__main__":
     app.run(debug=True)
